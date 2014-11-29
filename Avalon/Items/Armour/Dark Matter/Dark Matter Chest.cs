903b3473314f976c433e75948cb38f5db620f7c6
Bullseye55 <connersivley@gmail.com>
2014-11-29 15:14:36 -0600

    Fixed code.

--- Avalon/Items/Armour/Dark Matter/Dark Matter Chest.cs
+++ Avalon/Items/Armour/Dark Matter/Dark Matter Chest.cs
@@ -24,10 +24,10 @@ namespace Avalon.Items.Armour.DarkMatter
             p.invis = true;
 
                for (int i = 0; i < Main.gore.Length; i++)
-                   if (Vector2.DistanceSquared(Main.gore[i].position, p.Centre) < 7 * 7 && Main.gore[i].active = true)
+                   if (Vector2.DistanceSquared(Main.gore[i].position, p.Centre) < 100 * 100 && Main.gore[i].active == true)
                    {
                        int x = (int)(Main.gore[i].position.X / 16f), y = (int)(Main.gore[i].position.Y / 16f);
-                       Item.NewItem((int)p.position.X,(int)p.position.Y,(int)p.width,(int)p.height,ItemDef.byName["Avalon:Dark Matter Ooze"].type, 1, false, 0 );
+                       Item.NewItem((int)Main.gore[i].position.X, (int)Main.gore[i].position.Y, (int)Main.gore[i].scale, (int)Main.gore[i].scale, ItemDef.byName["Avalon:Dark Matter Ooze"].type, 1, false, 0);
                        Main.gore[i].active = false;
 
                    }
