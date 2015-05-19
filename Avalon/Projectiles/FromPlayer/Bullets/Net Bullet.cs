eae902813b3604154a8db67f9d1bb8b85a279510
Bullseye55 <connersivley@gmail.com>
2015-05-19 17:30:15 -0500

    Merge remote-tracking branch 'origin/Recent' into Recent
    Conflicts:
    	Avalon/Projectiles/FromPlayer/Bullets/Net Bullet.json
    	Avalon/Projectiles/FromPlayer/Bullets/Net.cs

--- Avalon/Projectiles/FromPlayer/Bullets/Net Bullet.cs
+++ Avalon/Projectiles/FromPlayer/Bullets/Net Bullet.cs
@@ -14,6 +14,11 @@ namespace Avalon.Projectiles.FromPlayer.Bullets
         /// <summary>
         ///
         /// </summary>
+        /// 
+        public override void AI()
+        {
+            projectile.damage = 0;
+        }
         public override void PostKill()
         {
             Projectile.NewProjectile(this.projectile.position, this.projectile.velocity, "Avalon:Net", 0, 0f);
--- Avalon/Projectiles/FromPlayer/Bullets/Net.cs
+++ Avalon/Projectiles/FromPlayer/Bullets/Net.cs
@@ -14,7 +14,7 @@ namespace Avalon.Projectiles.FromPlayer.Bullets
         /// <summary>
         ///
         /// </summary>
-        public override void AI()
+       /* public override void AI()
         {
             for (int i = 0; i < Main.npc.Length; i++)
                 if (projectile.Hitbox.Intersects(NPCDef.byType[i].Hitbox))
@@ -23,5 +23,35 @@ namespace Avalon.Projectiles.FromPlayer.Bullets
                     break;
                 }
         }
+       */
+        public override void AI()
+        {
+            foreach (NPC npc in Main.npc)
+                if (projectile.Hitbox.Intersects(npc.Hitbox))
+                {
+                    if (npc.active)
+                    {
+                        if (Main.netMode == 1)
+                        {
+                            npc.active = false;
+                            NetMessage.SendData(70, -1, -1, "", npc.whoAmI, (float)projectile.owner, 0.0f, 0.0f, 0);
+                        }
+                        else
+                        {
+                            if ((int)npc.catchItem > 0)
+                            {
+                                int num = npc.type;
+                                new Item().SetDefaults((int)npc.catchItem, false);
+                                Item.NewItem((int)npc.Center.X, (int)npc.Center.Y, 0, 0, (int)npc.catchItem, 1, false, 0, true);
+                                npc.active = false;
+                                NetMessage.SendData(23, -1, -1, "", npc.whoAmI, 0.0f, 0.0f, 0.0f, 0);
+                            }
+                        }
+                    }
+
+                    break;
+                }
+        } 
+         
     }
 }
